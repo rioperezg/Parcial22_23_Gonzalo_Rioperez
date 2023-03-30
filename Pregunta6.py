615cdc812a9d93af588fd6d7ede120c467d26507
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-30 09:36:32 +0200

    haciendo forma iterativa

--- Pregunta6.py
+++ Pregunta6.py
@@ -4,7 +4,26 @@ forma iterativa
 """
 # Primero haremos uan clase matrix que nos permita crear matrices 
 class Matrix:
-
+    def __init__(self, numeros):
+        self.numeros = numeros
+        self.filas = len(numeros)
+        self.columnas = len(numeros[0]) if numeros else 0
+    def obtener_elementos(self, fila, columna):
+        return self.numeros[fila][columna]
+    def obtener_fila(self, fila):
+        return self.numeros[fila]
+    def obtener_columna(self, columna):
+        return [self.numeros[fila][columna] for fila in range(self.filas)]
+    # Ahora pasamos a calcular determinantes mediante sarrus
+    def Dterminante_itera(self):
+        if self.filas == 2 and self.columnas == 2:
+            return self.numeros[0][0] * self.numeros[1][1] - self.numeros[0][1] * self.numeros[1][0]
+        else:
+            determinante = 0
+            for i in range(self.columnas):
+                matriz = Matrix([self.obtener_fila(fila) for fila in range(1, self.filas)])
+                determinante += (-1) ** i * self.obtener_elementos(0, i) * matriz.Dterminante_itera()
+            return determinante
 
 
 
