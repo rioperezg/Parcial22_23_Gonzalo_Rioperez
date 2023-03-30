2b9bd9d4c6434b8a999c29871db27e40805d6bef
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-30 08:52:45 +0200

    creada funcion imprimir

---
+++ Pregunta2.py
@@ -0,0 +1,109 @@
+""""
+Recorre el listado del ejemplo y realiza las siguientes operaciones:
+
+[18, 50, 210, 80, 145, 333, 70, 30]
+
+Imprimir el número en caso de que sea múltiplo de 10 y menor que 200
+Parar el programa si llega a un número mayor que 300
+Organizar la lista mediante el método de ordenamiento merge sort
+Dada la lista anterior y un valor 145 devolver el índice de 145 en la lista si 145 está en la lista, y −1 si 145 no está en la lista
+
+"""
+class nodoCola(object):
+    info, sig = None, None
+class Cola(object):
+    def __init__(self):
+        self.frente, self.final = None, None
+        self.tamaño = 0
+    def arribo(cola, dato):
+        nodo = nodoCola()
+        nodo.info = dato
+        if cola.frente is None:
+            cola.frente = nodo
+        else:
+            cola.final.sig = nodo
+        cola.final = nodo
+        cola.tamaño += 1
+    def atencion(cola, dato):
+        dato = cola.frente.info
+        cola.frente = cola.frente.sig
+        if cola.frente is None:
+            cola.final = None
+            cola.tamaño -= 1
+            return dato
+    def cola_vacia(cola):
+        return cola.frente is None
+    def en_frente(cola):
+        return cola.frente.info
+    def tamaño(cola):
+        return cola.tamaño
+    def mover_al_final(cola):
+        dato = Cola.atencion(cola)
+        Cola.arribo(cola,dato)
+        return dato
+    # Primera forma barrido, ineficiente(n^2)
+    def barrido(cola):
+        caux = Cola()
+        while(not Cola.cola_vacia(cola)):
+            dato = Cola.atencion(cola)
+            print(dato)
+            Cola.arribo(caux, dato)
+        while(not Cola.cola_vacia(caux)):
+            dato = Cola.atencion(caux)
+            Cola.arribo(cola,dato)
+    # Segunda forma barrido, eficiente(n)
+    def barrido2(cola):
+        i = 0
+        while(i < Cola.tamaño(cola)):
+            dato = Cola.mover_al_final(cola)
+            print(dato)
+            i += 1
+
+def Imprimir(listado):
+    # Para hacerlo de forma recursiva pondremos condiciones: hasta que la lista no este vacia....
+    listado = Cola()
+    if Cola.cola_vacia(listado) == True:
+        return "El listado esta vacio"
+    else:
+        dato = Cola.atencion(listado)
+        if dato % 10 == 0 and dato < 200:
+            print(dato)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+
+
+
+if __name__ == "__main__":
+main()
