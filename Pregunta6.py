426c330d9e00c105fcafe3e2c19df236f029b1ce
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-30 09:31:22 +0200

    creando clase de la matriz

--- Pregunta6.py
+++ Pregunta6.py
@@ -2,6 +2,8 @@
 Realiza el  código para calcular el determinante de una matriz cuadrada de [5 x 5], regla de Sarrus de forma recursiva y de 
 forma iterativa
 """
+# Primero haremos uan clase matrix que nos permita crear matrices 
+class Matrix:
 
 
 
