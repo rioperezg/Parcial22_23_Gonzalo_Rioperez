2f5d6c73c84f3af66919841f387b8c028056887a
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-30 09:06:42 +0200

    clase alumno creada

--- Pregunta3.py
+++ Pregunta3.py
@@ -1,28 +1,22 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
+Creación (0,5 puntos)
+Crea una clase llamada Alumno que tenga los atributos nombre y nota
+Crea el constructor de la clase. Añadir en el constructor un print para informar de que el alumno se ha creado con éxito
+Crear un método llamado calificación que imprima por pantalla si el alumno ha aprobado o suspendido en base a su nota
+Experimentación (0,5 Puntos)
+Crea algunos alumnos
+Prueba a ejecutar el método calificación de cada objeto que has creado
+"""
+class Alumno(object):
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+        print("El alumno se ha creado con éxito")
+    def calificacion(self):
+        if self.nota >= 5:
+            print("El alumno ha aprobado")
+        else:
+            print("El alumno ha suspendido")
 
 
 
