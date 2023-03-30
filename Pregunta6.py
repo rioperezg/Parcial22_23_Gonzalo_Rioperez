41c9fdbf46af394dc6b91b29218fbc49e7dde3df
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-30 09:45:23 +0200

    haciendo forma recursiva

--- Pregunta6.py
+++ Pregunta6.py
@@ -8,7 +8,7 @@ class Matrix:
         self.numeros = numeros
         self.filas = len(numeros)
         self.columnas = len(numeros[0]) if numeros else 0
-    def obtener_elementos(self, fila, columna):
+    def obtener_numeros(self, fila, columna):
         return self.numeros[fila][columna]
     def obtener_fila(self, fila):
         return self.numeros[fila]
@@ -24,8 +24,21 @@ class Matrix:
             determinante = 0
             for i in range(self.columnas):
                 matriz = Matrix([self.obtener_fila(fila) for fila in range(1, self.filas)])
-                determinante += (-1) ** i * self.obtener_elementos(0, i) * matriz.Dterminante_itera()
+                matriz.numeros = [[matriz.obtener_numeros(fila, columna) for columna in range(matriz.columnas) if columna != i] for fila in range(matriz.filas)]
+                determinante += (-1) ** i * self.obtener_numeros(0, i) * matriz.Dterminante_itera()
             return determinante
 
     def Dterminante_recursiva(self):
-        
+        determinante = 0
+        if self.filas == 0 and self.columnas == 0:
+            print("El calculo del determinante se ha terminado")
+            return determinante
+        else:
+            if self.filas == 2 and self.columnas == 2:
+                return self.numeros[0][0] * self.numeros[1][1] - self.numeros[0][1] * self.numeros[1][0]
+            elif self.filas != self.columnas:
+                return "No se puede calcular el determinante"
+            else:
+                determinante = 0
+            
+            
