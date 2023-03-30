1e584983cb14931a5370c3a61ce3d900fed6563d
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-30 09:15:17 +0200

    haciendo pregunta4

---
+++ Pregunta4.py
@@ -0,0 +1,50 @@
+""""
+Creación (0,5 puntos)
+Copia el ejercicio anterior y realicemos una modificación:
+
+Junto al método init y calificación, vamos a crear otro método especial de Python, el método str. Al igual que init, debe ir 
+encerrado entre dobles guiones bajos, y debe tener el siguiente formato:
+
+def __str__(self): return "Lo que quiero mostrar"
+
+Este método nos sirve para imprimir por pantalla la información de un objeto, si tenemos un objeto alumno1 creado y realizamos 
+print(alumno1), Python ejecutará el contenido del método str (el método str lo que tiene que hacer es maquetar la información que 
+desea en un string).
+
+Experimentación (0,5 puntos)
+Implementa el método str y haz que muestre el nombre y la nota del alumno
+Crea algun objeto de la clase Alumno
+Realiza print de esos objetos para visualizar por pantalla la información del str
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+main()
